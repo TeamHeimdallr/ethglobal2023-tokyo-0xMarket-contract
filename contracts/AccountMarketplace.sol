dd553aeb557ee34ba06ce93e3b84718a076bb95f
TAEKEON GO <taekeongo@gmail.com>
2023-04-15 10:50:02 +0900

    AccountMarketplace and it is tested

--- contracts/AccountMarketplace.sol
+++ contracts/AccountMarketplace.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 interface IAccount {
     function owner() external returns (address);
 
-    function trnasferOwnership(address) external;
+    function changeOwner(address) external;
 }
 
 contract AccountMarketplace {
@@ -14,10 +14,14 @@ contract AccountMarketplace {
     mapping(address => address) accountOwner;
     mapping(address => uint) accountPrice;
     mapping(address => address) payoutReceiver;
+    address[] accounts;
 
     constructor(address _tokenAddress) {
         tokenAddress = _tokenAddress;
     }
+
+    event ListAccount(address account, address receiver, uint price);
+    event BuyAccount(address account);
 
     /*
      * For listing an account
@@ -46,7 +50,16 @@ contract AccountMarketplace {
         );
         require(price != 0, "You can't list the account with price 0");
         accountPrice[account] = price;
-        payoutReceiver[account] = receiver;
+        payoutReceiver[account] = receiver == address(0)
+            ? msg.sender
+            : receiver;
+        accounts.push(account);
+
+        emit ListAccount(
+            account,
+            receiver == address(0) ? msg.sender : receiver,
+            price
+        );
     }
 
     function revoke(address account) external {
@@ -60,17 +73,31 @@ contract AccountMarketplace {
 
     function buy(address account) external {
         require(accountPrice[account] != 0, "The account is not listed");
+
+        // Need an approval in advance
         IERC20(tokenAddress).transferFrom(
             msg.sender,
             accountOwner[account],
             accountPrice[account]
         );
+
+        IAccount(account).changeOwner(msg.sender);
+        emit BuyAccount(account);
+
+        // Reset mappings
+        accountOwner[account] = msg.sender;
+        accountPrice[account] = 0;
+        payoutReceiver[account] = address(0);
     }
 
     function accountRegister(
         address account
     ) external view returns (address owner) {
         owner = accountOwner[account];
+    }
+
+    function getAccounts() external view returns (address[] memory) {
+        return accounts;
     }
 
     function listedPrice(address account) external view returns (uint price) {
